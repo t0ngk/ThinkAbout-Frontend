ac7158d29249faf615e34e2def48878b4949505d
Thavith Javisooth <spicedexdee123@gmail.com>
2024-03-11 19:47:30 +0700

    💄 : update choices componets

--- src/lib/Components/Choices.svelte
+++ src/lib/Components/Choices.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
 	import { createRadioGroup, melt } from '@melt-ui/svelte';
 
 	const {
@@ -8,6 +10,8 @@
 		defaultValue: 'default'
 	});
 
+	export let options = '';
+	console.log(Boolean(options));
 	const optionsArr = ['default', 'comfortable', 'compact'];
 </script>
 
@@ -16,7 +20,7 @@
 	class="grid w-full grid-cols-1 gap-x-4 gap-y-2 data-[orientation=horizontal]:grid-cols-2 md:grid-cols-2"
 	aria-label="View density"
 >
-	{#each optionsArr as option}
+	{#each options ? options : optionsArr as option}
 		<button
 			use:melt={$item(option)}
 			class={`w-full rounded-lg p-4 text-start transition-all
